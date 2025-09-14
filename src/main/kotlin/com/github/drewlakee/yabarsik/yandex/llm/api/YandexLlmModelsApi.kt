292533b937587ccc22504eadef3439466141d421
aleynik0vav <drew.lake@yandex.com>
2025-09-14 18:54:08 +0300

    Add YandexLlmModelsApi & AskGpt

---
+++ src/main/kotlin/com/github/drewlakee/yabarsik/yandex/llm/api/YandexLlmModelsApi.kt
@@ -0,0 +1,51 @@
+package com.github.drewlakee.yabarsik.yandex.llm.api
+
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import dev.forkhandles.result4k.Result4k
+import org.http4k.cloudnative.RemoteRequestFailed
+import org.http4k.connect.Action
+import org.http4k.core.Body
+import org.http4k.core.HttpHandler
+import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters.SetBaseUriFrom
+import org.http4k.filter.RequestFilters.SetHeader
+
+interface YandexLlmModelsApiAction<R>  : Action<Result4k<R, RemoteRequestFailed>> {
+
+    companion object  {
+        protected val JSON = ObjectMapper()
+            .registerKotlinModule()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+        protected inline fun <reified R> jsonTo(body: Body): R = JSON.readValue(body.stream)
+        protected fun toJsonString(data: Any): String = JSON.writeValueAsString(data)
+    }
+}
+
+interface YandexLlmModelsApi {
+    operator fun <R : Any> invoke(action: YandexLlmModelsApiAction<R>): Result4k<R, RemoteRequestFailed>
+
+    companion object
+}
+
+fun YandexLlmModelsApi.Companion.Http(client: HttpHandler) = object : YandexLlmModelsApi {
+    private val http = SetBaseUriFrom(Uri.of("https://llm.api.cloud.yandex.net"))
+        .then(SetHeader("Content-Type", "application/json"))
+        .then(SetHeader("Accept", "application/json"))
+        .then(SetHeader("Authorization", "Api-Key ${System.getenv("YANDEX_CLOUD_LLM_API_KEY")}"))
+        .then(client)
+
+    override fun <R : Any> invoke(action: YandexLlmModelsApiAction<R>): Result4k<R, RemoteRequestFailed> =
+        action.toResult(http(action.toRequest()))
+}
+
+enum class LlmMessageRole(@field:JsonValue val value: String) {
+    SYSTEM("system"),
+    USER("user"),
+    ASSISTANT("assistant"),
+}
